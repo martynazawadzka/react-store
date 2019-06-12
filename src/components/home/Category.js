861e048cb1d18d1882f1e25184d57b31a68866e2
martynazawadzka <zawadzka.m94@gmail.com>
2019-06-12 20:57:56 +0200

    fixed cart update on home page

--- src/components/home/Category.js
+++ src/components/home/Category.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Product from '../shared/Product';
+import Product from '../../containers/ProductContainer';
 
 const Category = ({title, products}) => {
   return (
